a4d1b1f0808c448e2e487239bad843e2f92665bd
okonomi <okonomi@oknm.jp>
2020-09-28 19:07:30 +0900

    invoke after create

--- web-screenshot-webapp/app/models/screenshot.rb
+++ web-screenshot-webapp/app/models/screenshot.rb
@@ -1,3 +1,19 @@
 class Screenshot < ApplicationRecord
   validates :url, presence: true
+
+  after_create :invoke_screenshot
+
+  private
+
+  def invoke_screenshot
+    client = Aws::Lambda::Client.new(region: 'us-east-1')
+    client.invoke(
+      function_name: 'web-screenshot-serverless-dev-screenshot',
+      payload: {
+        id: id,
+        url: url
+      }.to_json,
+      invocation_type: 'Event',
+    )
+  end
 end
